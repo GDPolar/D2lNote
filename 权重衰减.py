833a1458cae49ff5502ed31700ed1b399d50e07b
GD <2249303992@qq.com>
2022-09-22 17:28:24 +0800

    update

---
+++ 权重衰减.py
@@ -0,0 +1,45 @@
+# !pip install git+https://github.com/d2l-ai/d2l-zh@release  # installing d2l
+# %matplotlib inline
+
+import random
+import torch
+from d2l import torch as d2l
+
+# 为出现过拟合，将 n_train 设置小些，num_inputs 设置大些
+n_train, n_test, num_inputs, batch_size = 20, 100, 200, 5
+true_w, true_b = torch.ones((num_inputs, 1)) * 0.01, 0.05
+train_data = d2l.synthetic_data(true_w, true_b, n_train)
+train_iter = d2l.load_array(train_data, batch_size)
+test_data = d2l.synthetic_data(true_w, true_b, n_test)
+test_iter = d2l.load_array(test_data, batch_size, is_train=False)
+
+def init_params():
+    w = torch.normal(0, 1, size=(num_inputs, 1), requires_grad=True)
+    b = torch.zeros(1, requires_grad=True)
+    return [w, b]
+
+# 定义L2范数惩罚，对所有项求平方后并将它们求和。
+def l2_penalty(w):
+    return torch.sum(w.pow(2)) / 2
+
+def train(lambd):
+    w, b = init_params()
+    net, loss = lambda X: d2l.linreg(X, w, b), d2l.squared_loss
+    num_epochs, lr = 100, 0.003
+    animator = d2l.Animator(xlabel='epochs', ylabel='loss', yscale='log',
+                            xlim=[5, num_epochs], legend=['train', 'test'])
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            # 增加了L2范数惩罚项，
+            # 广播机制使l2_penalty(w)成为一个长度为batch_size的向量
+            l = loss(net(X), y) + lambd * l2_penalty(w)
+            l.sum().backward()
+            d2l.sgd([w, b], lr, batch_size)
+        if (epoch + 1) % 5 == 0:
+            animator.add(epoch + 1, (d2l.evaluate_loss(net, train_iter, loss),
+                                     d2l.evaluate_loss(net, test_iter, loss)))
+    print('w的L2范数是：', torch.norm(w).item())
+
+train(lambd=0) # 严重过拟合
+
+train(lambd=10) # 有所缓解
