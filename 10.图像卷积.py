7bd085cdff0e4be423da14e4bc16077c51ca2d62
GDPolar <2249303992@qq.com>
2022-09-25 23:09:13 +0800

    update

---
+++ 10.图像卷积.py
@@ -0,0 +1,70 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+# X 输入张量， K 卷积核张量
+def corr2d(X, K):  
+    """计算二维互相关运算"""
+    h, w = K.shape
+    # 计算输出尺寸
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i:i + h, j:j + w] * K).sum()
+    return Y
+
+# 定义卷积层
+class Conv2D(nn.Module):
+    def __init__(self, kernel_size):
+        super().__init__()
+        self.weight = nn.Parameter(torch.rand(kernel_size))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        return corr2d(x, self.weight) + self.bias
+
+# 构造一个 6 * 8 像素的黑白图像。中间四列为黑色（0），其余像素为白色（1）
+X = torch.ones((6, 8))
+X[:, 2:6] = 0
+X
+'''
+tensor([[1., 1., 0., 0., 0., 0., 1., 1.],
+        [1., 1., 0., 0., 0., 0., 1., 1.],
+        [1., 1., 0., 0., 0., 0., 1., 1.],
+        [1., 1., 0., 0., 0., 0., 1., 1.],
+        [1., 1., 0., 0., 0., 0., 1., 1.],
+        [1., 1., 0., 0., 0., 0., 1., 1.]])
+'''
+# 通过构造出的核进行互相关运算时，如果水平相邻的两元素相同，则输出为零，否则输出为非零
+K = torch.tensor([[1.0, -1.0]])
+Y = corr2d(X, K)
+Y
+'''
+tensor([[ 0.,  1.,  0.,  0.,  0., -1.,  0.],
+        [ 0.,  1.,  0.,  0.,  0., -1.,  0.],
+        [ 0.,  1.,  0.,  0.,  0., -1.,  0.],
+        [ 0.,  1.,  0.,  0.,  0., -1.,  0.],
+        [ 0.,  1.,  0.,  0.,  0., -1.,  0.],
+        [ 0.,  1.,  0.,  0.,  0., -1.,  0.]])
+'''
+
+# 尝试学习核的参数
+# 构造一个二维卷积层，它具有1个输出通道和形状为（1，2）的卷积核
+conv2d = nn.Conv2d(1,1, kernel_size=(1, 2), bias=False)
+
+# 这个二维卷积层使用四维输入和输出格式（批量大小、通道、高度、宽度），
+# 其中批量大小和通道数都为1
+X = X.reshape((1, 1, 6, 8))
+Y = Y.reshape((1, 1, 6, 7))
+lr = 3e-2  # 学习率
+
+for i in range(10):
+    Y_hat = conv2d(X)
+    l = (Y_hat - Y) ** 2
+    conv2d.zero_grad()
+    l.sum().backward()
+    # 迭代卷积核
+    conv2d.weight.data[:] -= lr * conv2d.weight.grad
+    if (i + 1) % 2 == 0:
+        print(f'epoch {i+1}, loss {l.sum():.3f}')
+conv2d.weight.data.reshape((1, 2))
